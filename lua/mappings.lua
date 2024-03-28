f80a2ae2f0696158e14eb926a4c16df9f5e7c14b
Peter Xu <p@cooliktas.com>
2024-03-28 13:28:45 +0800

    update

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -14,21 +14,24 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "Escape insert mode" })
 ------------------------------------------------------------
 local move_opts = { noremap = true, silent = true }
 -- Normal-mode commands
-vim.keymap.set('n', '<A-j>', ':MoveLine(1)<CR>', move_opts)
-vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<CR>', move_opts)
-vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<CR>', move_opts)
-vim.keymap.set('n', '<A-l>', ':MoveHChar(1)<CR>', move_opts)
-vim.keymap.set('n', '<leader>mf', ':MoveWord(1)<CR>', move_opts)
-vim.keymap.set('n', '<leader>mb', ':MoveWord(-1)<CR>', move_opts)
+vim.keymap.set("n", "<A-j>", ":MoveLine(1)<CR>", move_opts)
+vim.keymap.set("n", "<A-k>", ":MoveLine(-1)<CR>", move_opts)
+vim.keymap.set("n", "<A-h>", ":MoveHChar(-1)<CR>", move_opts)
+vim.keymap.set("n", "<A-l>", ":MoveHChar(1)<CR>", move_opts)
+vim.keymap.set("n", "<leader>mf", ":MoveWord(1)<CR>", move_opts)
+vim.keymap.set("n", "<leader>mb", ":MoveWord(-1)<CR>", move_opts)
 -- Visual-mode commands
-vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<CR>', move_opts)
-vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<CR>', move_opts)
-vim.keymap.set('v', '<A-h>', ':MoveHBlock(-1)<CR>', move_opts)
-vim.keymap.set('v', '<A-l>', ':MoveHBlock(1)<CR>', move_opts)
-
+vim.keymap.set("v", "<A-j>", ":MoveBlock(1)<CR>", move_opts)
+vim.keymap.set("v", "<A-k>", ":MoveBlock(-1)<CR>", move_opts)
+vim.keymap.set("v", "<A-h>", ":MoveHBlock(-1)<CR>", move_opts)
+vim.keymap.set("v", "<A-l>", ":MoveHBlock(1)<CR>", move_opts)
 
 ------------------------------------------------------------
--- for overriding `fedepujol/move.nvim` 
+-- for overriding `fedepujol/move.nvim`
 ------------------------------------------------------------
-vim.keymap.set('n', '<space>rs', vim.lsp.buf.rename, { desc = "Refactore: Rename Symbol"})
-
+vim.keymap.set("n", "<space>rs", vim.lsp.buf.rename, { desc = "Refactore: Rename Symbol" })
+vim.keymap.set("n", "<space>ts", function()
+	if vim.lsp.inlay_hint then
+		vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+	end
+end, { desc = "Toggle Inlay Hints" })
