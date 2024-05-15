e3c91804edf1c8fee9b1964f5bf9ed683c469d3c
Peter Xu <p@cooliktas.com>
2024-05-15 12:07:20 +0800

    add nvim-nio

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -139,7 +139,7 @@ local plugins = {
 	},
 	{
 		"rcarriga/nvim-dap-ui",
-		dependencies = "mfussenegger/nvim-dap",
+		dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
 		config = function()
 			local dap, dapui = require("dap"), require("dapui")
 			dapui.setup()
