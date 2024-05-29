aeebb5f62fe53a158635407b3ae07a604a624a2d
Peter Xu <p@cooliktas.com>
2024-05-29 11:37:58 +0800

    update init.lua for neovide

--- init.lua
+++ init.lua
@@ -40,4 +40,13 @@ end)
 
 
 -- add some configs for neovide
+
+-- allow Meta
 vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+
+-- Allow clipboard copy paste in neovim
+vim.g.neovide_input_use_logo = 1
+vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
