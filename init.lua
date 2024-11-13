0c814a962407e4b2714f42b6cf19ef64bd697804
Peter Xu <p@cooliktas.com>
2024-11-13 16:14:43 +0800

    add font resize

--- init.lua
+++ init.lua
@@ -5,8 +5,8 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-	local repo = "https://github.com/folke/lazy.nvim.git"
-	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -15,17 +15,17 @@ local lazy_config = require("configs.lazy")
 
 -- load plugins
 require("lazy").setup({
-	{
-		"NvChad/NvChad",
-		lazy = false,
-		branch = "v2.5",
-		import = "nvchad.plugins",
-		config = function()
-			require("options")
-		end,
-	},
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",
+    config = function()
+      require("options")
+    end,
+  },
 
-	{ import = "plugins" },
+  { import = "plugins" },
 }, lazy_config)
 
 -- load theme
@@ -35,18 +35,28 @@ dofile(vim.g.base46_cache .. "statusline")
 require("nvchad.autocmds")
 
 vim.schedule(function()
-	require("mappings")
+  require("mappings")
 end)
 
-
--- add some configs for neovide
-
--- allow Meta
-vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
-
--- Allow clipboard copy paste in neovim
-vim.g.neovide_input_use_logo = 1
 vim.api.nvim_set_keymap('', '<D-v>', '+p<CR>', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('!', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('t', '<D-v>', '<C-R>+', { noremap = true, silent = true})
 vim.api.nvim_set_keymap('v', '<D-v>', '<C-R>+', { noremap = true, silent = true})
+
+-- add some configs for neovide
+if vim.g.neovide then
+  -- allow Meta
+  vim.g.neovide_input_macos_option_key_is_meta = 'only_left'
+  -- Allow clipboard copy paste in neovim
+  vim.g.neovide_input_use_logo = 1
+  -- Enable Command + and Command - to resize fonts
+  vim.keymap.set("n", "<D-=>", function ()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1
+  end, { noremap = true, silent = true })
+  vim.keymap.set("n", "<D-->", function ()
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1
+  end, { noremap = true, silent = true })
+  vim.keymap.set("n", "<D-0>", function ()
+    vim.g.neovide_scale_factor = 1
+  end, { noremap = true, silent = true })
+end
